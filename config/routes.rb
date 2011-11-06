840fe7cef1c77be1c447f90c057cac71c962c7f5
Waley Chen <nikeelevet@hotmail.com>
2011-11-06 04:51:56 -0500

    back up

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ TwitterClone::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
   root :to => "frontpage#frontpage"
-  resources :users
+  resources :users, controller:"user"
   match "frontpage" => "frontpage#frontpage"
   match "sign_up" => "user#sign_up"
   match "show" => "user#show"
