9be737dc9cd4914e829cd1edf8e72ed3e02f0fad
Maverick <maverick.lee225@gmail.com>
2012-01-08 02:44:23 -0500

    Allow for adding of nodes and new branches

--- app/controllers/story_nodes_controller.rb
+++ app/controllers/story_nodes_controller.rb
@@ -5,25 +5,37 @@ class StoryNodesController < ApplicationController
   end
 
   def update
-    puts params
-    puts params[:title]
-    puts params[:content]
     @story_node = StoryNode.find(params[:id])
     @story_node.title = params[:story_node][:title]
     @story_node.content = params[:story_node][:content]
     @story_node.save
 
     render 'view'
-
   end
 
   def new
+    @story_node = StoryNode.new
+    render 'new'
+  end
 
+  def create
+    @story_node = StoryNode.new
+    @story_node.title = params[:story_node][:title]
+    @story_node.content = params[:story_node][:content]
+    @story_node.save
+
+    @story_branch = StoryBranch.new
+    @story_branch.from_node = params[:story_node][:referer]
+    @story_branch.to_node = @story_node.id
+    @story_branch.save
+
+    render 'view'
   end
 
   def show
     @story_node = StoryNode.find(params[:id])
-    #@story_branches = StoryBranch.find(@story_node.id)
+    @story_branches = StoryBranch.where(:from_node => @story_node.id)
+    @branch_titles = StoryNode.all(:select => "title", :conditions => ["id in (?)", @story_branches.collect {|x| x.to_node }])
 
     render 'view'
   end
