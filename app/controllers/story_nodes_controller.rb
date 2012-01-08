dd37dc886ceb06343abc29298271eb8c3b26b59a
Maverick <maverick.lee225@gmail.com>
2012-01-08 00:46:37 -0500

    Creates the story node when making a new story and allows for saving

--- app/controllers/story_nodes_controller.rb
+++ app/controllers/story_nodes_controller.rb
@@ -4,7 +4,25 @@ class StoryNodesController < ApplicationController
     render 'edit'
   end
 
-  def save
+  def update
+    puts params
+    puts params[:title]
+    puts params[:content]
+    @story_node = StoryNode.find(params[:id])
+    @story_node.title = params[:story_node][:title]
+    @story_node.content = params[:story_node][:content]
+    @story_node.save
 
+    render 'view'
+
+  end
+
+  def new
+
+  end
+
+  def show
+    @story_node = StoryNode.find(params[:id])
+    render 'view'
   end
 end
