262415cfe056b05ed6c91b6ecd82210111fe2b4f
Maverick <maverick.lee225@gmail.com>
2012-01-08 12:35:49 -0500

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120108170039) do
+ActiveRecord::Schema.define(:version => 20120108173420) do
 
   create_table "stories", :force => true do |t|
     t.string   "title"
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(:version => 20120108170039) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "story_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "user_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
